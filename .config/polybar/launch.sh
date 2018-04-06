0c8404c745d04719296d670b1a39d926d32a2821
Maha <maha.elbayad@gmail.com>
2018-04-06 17:58:30 +0200

    :up: auto

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,7 +4,7 @@
 killall -q polybar
 #while pgrep -x polybar >/dev/null; do sleep 1; done 
 #nitrogen --set-auto ~/Pictures/galaxies_stars_sky_dark_nebula_101228_1920x1080.jpg
-nitrogen --set-auto ~/Pictures/galaxy_nebula_blurring_stars_65152_1920x1080.jpg   
+nitrogen --set-auto ~/Pictures/paint_stains_purple_118346_1920x1080.jpg
 
 
 
